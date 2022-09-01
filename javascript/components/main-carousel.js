6fb6e24d61f2fccb7ff021b6d54744d12f97cb4d
Kaio Novaes <kaionicollas1515@gmail.com>
2022-09-01 13:09:21 -0300

    Component updated - JS = main-carousel.js

--- javascript/components/main-carousel.js
+++ javascript/components/main-carousel.js
@@ -4,11 +4,11 @@ const elements = document.querySelector('.elements');
 let pixels = 50;
 
 btnRight.addEventListener('click', function() {
-    pixels = pixels + 50;
+    pixels = pixels - 50;
     elements.style = `transform: translateX(${pixels}px)`;
 });
 
 btnLeft.addEventListener('click', function() {
-    pixels = pixels - 50;
+    pixels = pixels + 50;
     elements.style = `transform: translateX(${pixels}px)`;
 });
