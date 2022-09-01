510b5435a97a9b445dd44f1eefa94267aa5fc089
Kaio Novaes <kaionicollas1515@gmail.com>
2022-09-01 12:49:30 -0300

    Component updated - JS = main-carousel.js

--- javascript/components/main-carousel.js
+++ javascript/components/main-carousel.js
@@ -1,1 +1,14 @@
-// alert('Caixa troll');
+const btnRight = document.querySelector('.button-arrow.-right');
+const btnLeft = document.querySelector('.button-arrow.-left');
+const elements = document.querySelector('.elements');
+let pixels = 50;
+
+btnRight.addEventListener('click', function() {
+    pixels = pixels + 50;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+
+btnLeft.addEventListener('click', function() {
+    pixels = pixels - 50;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
